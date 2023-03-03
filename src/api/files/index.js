d4ae85bf6dcba8a13c5024a5f0bc59dcee7fc6a7
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-03-03 16:15:33 +0100

    files upload

--- src/api/files/index.js
+++ src/api/files/index.js
@@ -1,6 +1,12 @@
 import Express from "express";
+import createHttpError from "http-errors";
 import multer from "multer";
-import { saveProductImg } from "../../lib/fs-tools.js";
+import { extname } from "path";
+import {
+  getProducts,
+  saveProductImg,
+  writeProducts,
+} from "../../lib/fs-tools.js";
 
 const filesRouter = Express.Router();
 
@@ -9,7 +15,32 @@ filesRouter.post(
   multer().single("image"),
   async (req, res, next) => {
     try {
-      await saveProductImg();
+      const allProducts = await getProducts();
+      const productId = req.params.productsId;
+      const index = allProducts.findIndex(
+        (product) => product.id === productId
+      );
+      if (index !== -1) {
+        console.log(req.file);
+        const originalFileExt = extname(req.file.originalname);
+        const fileName = productId + originalFileExt;
+        await saveProductImg(fileName, req.file.buffer);
+
+        const oldProduct = allProducts[index];
+        const updatedProduct = {
+          ...oldProduct,
+          ...req.body,
+          imageURL: `http://localhost:3001/img/${fileName}`,
+          updatedAt: new Date(),
+        };
+        allProducts[index] = updatedProduct;
+        await writeProducts(allProducts);
+        res.send({ message: "File has been uploaded" });
+      } else {
+        next(
+          createHttpError(404, `Product with the id: ${productId} not found.`)
+        );
+      }
     } catch (error) {
       next(error);
     }
--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -7,7 +7,7 @@ const { readJSON, writeJSON, writeFile } = fs;
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
 const productsJSONPath = join(dataFolderPath, "products.json");
 const reviewsJSONPath = join(dataFolderPath, "reviews.json");
-const imgPublicFolderPath = join(process.cwd(), ".public/img");
+const imgPublicFolderPath = join(process.cwd(), "./public/img");
 
 export const getProducts = () => readJSON(productsJSONPath);
 export const writeProducts = (allProducts) =>
